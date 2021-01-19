d46e02b2529de8b240f9b7a565b473729f2e3641
CchronicJ <56306737+CchronicJ@users.noreply.github.com>
2021-01-19 10:13:31 -0500

    vhvjh
       mnmn b

---
+++ index.js
@@ -0,0 +1,21 @@
+const Websocket = require('ws');
+
+const app = new Websocket.Server({
+  port: 81
+})
+
+let clients = [];
+
+function broadcast(clientList, msgToBroadcast) {
+  clientList.forEach(function(client) {
+    client.send(msgToBroadcast);
+  })
+}
+app.on("connection", function(wsc, req) {
+  console.log("connection made!");
+  clients.push(wsc);
+  wsc.on("message", function(message) {
+    console.log(message);
+    broadcast(clients, message);
+  });
+})
